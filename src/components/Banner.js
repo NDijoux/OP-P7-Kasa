8150ff6a8f947d88a16ce63bf9b4493f70ca61a6
Nath <nathanael.dijouxpro@gmail.com>
2022-11-17 08:10:20 +0100

    Patch : review 17_11_22

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,25 @@
-import '../style/components/Banner.css'
-import logo from '../assets/logo.png'
+import React from 'react';
+import '../style/components/Banner.css';
+import logo from '../assets/logo.png';
+import { link } from 'react-router-dom';
+
 
 function Banner() {
     return (
-         <div className='headerContainer'>
-            <img src={logo} alt='Kasa' className='headerLogo' />
-            <p className='headerWelcome'>Accueil</p>
-            <p className='headerAbout'>A propos</p>
+        <div className='headerContainer'>
+            <nav>
+                <img src={logo} alt='Kasa' className='headerLogo' />
+                <ul>
+                    <li>
+                        <link className='headerLink'>Accueil</link>
+                    </li>
+                    <li>
+                        <link className='headerLink'>A propos</link>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    )
+    );
 }
 
 export default Banner
